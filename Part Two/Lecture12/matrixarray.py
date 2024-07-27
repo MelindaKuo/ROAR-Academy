6e43ca6d9502b4b2d582276ee23bcf22bb5f3d91
MelindaKuo <115421175+MelindaKuo@users.noreply.github.com>
2024-07-26 17:48:20 -0700

    update matrixarray.py

--- Part Two/Lecture12/matrixarray.py
+++ Part Two/Lecture12/matrixarray.py
@@ -2,5 +2,23 @@ import numpy as np
 
 
 def set_array(L, rows, cols):
-    arr = np.array()
-    
+    if len(L) != rows*cols:
+        print("Length doesn't match")
+    arr = np.empty((rows, cols))
+    index= 0
+    for i in range(rows):
+        for j in range(cols):
+            arr[i,j] = L[index]
+            index+=1
+    return arr
+
+
+L = [1, 2, 3, 4, 5, 6]
+rows, cols = 2, 3
+
+returned_arr = set_array(L, rows, cols)
+print("Array:")
+print(returned_arr)
+
+        
+
